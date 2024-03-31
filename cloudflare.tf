a6e8ead9fb96b20b8ef63fd80de1c96f71fefc1a
Alexis Yushin <alexis@apexive.com>
2024-03-31 14:48:23 -0400

    initial version

---
+++ cloudflare.tf
@@ -0,0 +1,19 @@
+data "cloudflare_zones" "zones" {
+  filter {}
+}
+
+locals {
+  zones = {for zone in data.cloudflare_zones.zones.zones : zone.name => zone.id}
+}
+
+resource "cloudflare_record" "cname" {
+  for_each = var.hostnames
+  name     = each.key
+  type     = "CNAME"
+  proxied  = false
+  value    = aws_cloudfront_distribution.cdn.domain_name
+  zone_id  = local.zones[each.value]
+  lifecycle {
+    ignore_changes = [zone_id]
+  }
+}
